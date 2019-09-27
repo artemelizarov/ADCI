a9326e565095cbfc26023c7480c8c990544860d8
Artem <46988814+artemelizarov@users.noreply.github.com>
2019-09-27 10:44:34 +0300

    Add files via upload

---
+++ test/js/script.js
@@ -0,0 +1,52 @@
+let sliderImages = {
+    'dot1': "url('./img/slider-img1-cutted.jpg')",
+    'dot2': "url('./img/main-img-full.jpg')",
+    'dot3': "url('./img/slider-img2-cutted.jpg')"
+};
+
+let prevSlider = '#dot2';
+
+$(document).ready(function(){
+
+    /* Slide main images */
+    $(".dot").click(function(event){
+        $(prevSlider).removeClass("active");
+        $(".main").css("background-image", sliderImages[event.target.id]);
+        $(event.target).addClass("active");
+        prevSlider = '#' + event.target.id;
+    });
+
+
+    /* Hide and show articles at the bottom of the main */
+    $(".hide-button").click(function(){
+        $(".articles-block").slideUp(200);
+    });
+
+    $(".show-button").click(function(){
+        $(".articles-block").slideDown(200);
+    });
+
+
+    /* Open and close the modal window with authentication */
+    $(".auth-area").click(function(){
+        $(".auth-window").addClass("active");
+        $("body").addClass("modal-open");
+    });
+
+    var closeModal = function() {
+        $(".auth-window").removeClass("active");
+        $("body").removeClass("modal-open");
+    }
+
+    $("#close-modal").click(function(){
+        closeModal();
+    });
+
+    $(window).click(function(event){
+        let modal = $(".auth-window");
+        if (event.target == modal[0]) {
+            closeModal();
+        }
+    });
+
+});
